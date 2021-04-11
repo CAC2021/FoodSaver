5d3a91194f646805a8ce3eff2dc2a34f6b9cf25c
e033005 <ericwfeng@gmail.com>
2021-04-11 11:41:19 -0400

    request post api

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.myapplication;
-
 import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.ContextWrapper;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -27,8 +28,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.net.URL;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,27 +89,52 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("b4");
             try {
                 String filename = "photo.bmp";
-                //System.out.println(getApplicationContext().getCacheDir());
-                File f = new File(filename);
+                File f = new File(getApplicationContext().getCacheDir(), filename);
                 f.createNewFile();
-
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                imageBitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
-                byte[] bitMapData = bos.toByteArray();
-
+                System.out.println("filename"+f);
+//                ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 FileOutputStream fos = new FileOutputStream(f);
-                fos.write(bitMapData);
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 0, fos);
+                System.out.println(imageBitmap);
+//                byte[] bitMapData = bos.toByteArray();
+//                System.out.println(bitMapData.length);
+//                Bitmap decodedByte = BitmapFactory.decodeByteArray(bitMapData, 0,bitMapData.length); //image
+//                ImageView image = (ImageView) findViewById(R.id.picview);
+//                image.setImageBitmap(decodedByte);
                 fos.flush();
                 fos.close();
+                upload("https://api3-production.up.railway.app/api/image/" ,f);
+
+
+//                try {
+//                    fos.write(bitMapData);
+//                    System.out.println("good");
+//                }
+//                catch(Exception e){
+//                    System.out.println("Bad");
+//                }
+
                 System.out.println("Worked :)");
             }
-            catch (IOException e) {
+            catch (Exception e) {
                 System.out.println("Fatal error.");
                 System.exit(0);
             }
         }
     }
-
+    public void upload(String url, File file) throws IOException {
+        System.out.println("sdf");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", file.getName(),
+                        RequestBody.create(file, MediaType.parse("multipart/form-data")))
+                .build();
+        System.out.println(formBody);
+        Request request = new Request.Builder().url(url).post(formBody).build();
+        Response response = client.newCall(request).execute();
+//        System.out.println(response);
+    }
     public void takePicture(View view) {
         dispatchTakePictureIntent();
     }
